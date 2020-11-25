9a1c4bccfae259a491d6c25df618ca43ea21edb1
Porostik <porostik2@yandex.ru>
2020-11-25 23:57:26 +0300

    добавил адаптив в шапку

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,8 +1,22 @@
 const burger = document.querySelector('.burger');
+const navPopup = document.querySelector('.popup-list');
+const navItemPopupList = document.querySelector('.nav-with-drop-list');
 const headerNav = document.querySelector('.header-list-wrapp');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   headerNav.classList.toggle('active');
   document.body.style.overflow = document.body.style.overflow ? '' : 'hidden';
+});
+
+navItemPopupList.addEventListener('click', (e) => {
+  if (e.target.parentElement !== navPopup) {
+    navPopup.classList.toggle('active');
+  }
+});
+
+document.body.addEventListener('click', (e) => {
+  if (!e.path.includes(navItemPopupList)) {
+    navPopup.classList.remove('active');
+  }
 });
